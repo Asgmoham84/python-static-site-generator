5c30167eb74208454d291099c31956192596145c
asgmoham84 <asgmoham84@gmail.com>
2022-04-13 10:16:37 -0400

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,11 +1,11 @@
 import shutil
+
 from typing import List
 from pathlib import Path
 
 
-class Parser():
-    def __init__(self, extensions: List[str] = []):
-        self.extensions = extensions
+class Parser:
+    extensions: List[str] = []
 
     def valid_extension(self, extension):
         return extension in self.extensions
@@ -14,12 +14,12 @@ class Parser():
         raise NotImplementedError
 
     def read(self, path):
-        with open(path, 'r') as file:
+        with open(path, "r") as file:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-        with open(full_path, 'w') as file:
+        with open(full_path, "w") as file:
             file.write(content)
 
     def copy(self, path, source, dest):
@@ -27,9 +27,7 @@ class Parser():
 
 
 class ResourceParser(Parser):
-        extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-    def parse(self, path: Path, source: Path, dest: Path):
+    def parse(self, path, source, dest):
         self.copy(path, source, dest)
-
-
--- ssg/site.py
+++ ssg/site.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-import os
 
-class Site():
+
+class Site:
     def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
@@ -10,14 +10,6 @@ class Site():
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
-
-    def build(self):
-        self.dest.mkdir(parents=True, exist_ok=True)
-        for path in self.source.rglob("*"):
-            if path.is_dir():
-                self.create_dir(path)
-            elif path.is_file():
-                self.run_parser(path)
 
     def load_parser(self, extension):
         for parser in self.parsers:
@@ -31,6 +23,10 @@ class Site():
         else:
             print("Not Implemented")
 
-
-
-
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
