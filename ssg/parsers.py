7b080565c3d0996e280578b25a4b043313d262bd
asgmoham84 <asgmoham84@gmail.com>
2022-04-13 10:15:14 -0400

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -18,7 +18,7 @@ class Parser():
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
-        full_path = self.dest / path.with_suffix(ext).name
+        full_path = dest / path.with_suffix(ext).name
         with open(full_path, 'w') as file:
             file.write(content)
 
@@ -27,8 +27,7 @@ class Parser():
 
 
 class ResourceParser(Parser):
-    def __init__(self, extensions):
-        self.extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+        extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path: Path, source: Path, dest: Path):
         self.copy(path, source, dest)
