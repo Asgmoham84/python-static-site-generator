1a5b84903573eb96734da6a8d7b2046825dcf478
asgmoham84 <asgmoham84@gmail.com>
2022-04-13 09:49:22 -0400

    Module 3 of project finished

--- ssg.py
+++ ssg.py
@@ -1,9 +1,9 @@
 import typer
 from ssg.site import Site
-
+import ssg.parsers
 
 def main(source="cpntent", dest="dist"):
-    config = {"source": source, "dest": dest}
+    config = {"source": source, "dest": dest, "parsers":[ssg.parsers.ResourceParser()]}
     site = Site(source=config["source"], dest=config["dest"])
     site.build()
 
--- ssg/site.py
+++ ssg/site.py
@@ -2,9 +2,10 @@ from pathlib import Path
 import os
 
 class Site():
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
@@ -12,9 +13,24 @@ class Site():
 
     def build(self):
         self.dest.mkdir(presents=True, exist_ok=True)
-        for  path in self.source.rglob("*"):
+        for path in self.source.rglob("*"):
             if os.path.isdir(path):
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
+
 
 
 
